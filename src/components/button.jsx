6d9c41ac1ccd691b6c68efbe1dffedbedee5085d
Aundrei Bryant <drei97@gmail.com>
2023-02-06 21:22:46 -0600

    feat: button component

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,12 +1,25 @@
-export default function Button() {
+import PropTypes from "prop-types";
+
+export default function Button({ type, color, text }) {
   return (
     <>
       <button
-        type="submit"
-        className="hover-bg-green-600 rounded bg-green-500 px-4 py-2 text-white"
+        type="{type}"
+        className={`rounded${color}-500px-4 py-2 text-white hover:${color}-600`}
       >
-        Register
+        {text}
       </button>
     </>
   );
 }
+
+Button.defaultProps = {
+  color: "bg-green",
+  type: "button",
+};
+
+Button.propTypes = {
+  type: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+};
