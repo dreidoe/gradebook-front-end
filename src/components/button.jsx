d6069933080178e6b7f4c6764c2f42824325f387
Aundrei Bryant <drei97@gmail.com>
2023-02-06 21:07:59 -0600

    button component created

---
+++ src/components/button.jsx
@@ -0,0 +1,12 @@
+export default function Button() {
+  return (
+    <>
+      <button
+        type="submit"
+        className="hover-bg-green-600 rounded bg-green-500 px-4 py-2 text-white"
+      >
+        Register
+      </button>
+    </>
+  );
+}
